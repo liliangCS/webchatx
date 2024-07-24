bd74465ae8e698db113b2cdb696ae19cb4e98fd9
liliang <1220795758@qq.com>
2024-07-24 14:50:35 +0800

    feat: added database table users and registration_codes

---
+++ 数据库设计/MySQL.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    nickname VARCHAR(50),
+    password_hash VARCHAR(255) NOT NULL,
+    user_level TINYINT NOT NULL DEFAULT 1, -- 管理员：0 普通用户：1
+    last_online_time DATETIME,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    code_id INT -- 注册码id
+);
+
+CREATE TABLE registration_codes (
+    code_id INT AUTO_INCREMENT PRIMARY KEY,
+    registration_code VARCHAR(255) NOT NULL UNIQUE,
+    total_usage_count INT NOT NULL,
+    current_usage_count INT NOT NULL DEFAULT 0,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    user_id INT NOT NULL, -- 用户id
+);
